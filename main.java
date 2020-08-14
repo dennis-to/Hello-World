78fb902775ba58fe8f772e293faf1c91ee71b973
Dennis To <dnnst3638@gmail.com>
2020-08-13 23:32:58 -0700

    add main.java and main.class

---
+++ main.java
@@ -0,0 +1,11 @@
+public class main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Dennis and I am a senior.");
+		System.out.println("One hobby I recently picked up is mechanical keyboards.");
+		System.out.println("I am applying to a few colleges in order to become a dentist.");
+		System.out.println("I mainly rhythm games, but I occasionally play JRPGs.");
+		System.out.println("I hope to understand computer science fundamentals by taking this class.");
+	}
+}
